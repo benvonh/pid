2bc0b8754bfe20588e50c38f284f26c7bac9b191
benvonh <benjaminvonsnarski@gmail.com>
2022-12-07 00:25:26 +1000

    Add documentation

--- pid/controller/controller.hpp
+++ pid/controller/controller.hpp
@@ -1,21 +1,46 @@
+/**
+ * @file controller.hpp
+ * @author Benjamin von Snarski (benjaminvonsnarski@gmail.com)
+ * @brief Header file for controller definition.
+ * @version 0.1
+ * @date 06-12-2022
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef __CONTROLLER_HPP__
 #define __CONTROLLER_HPP__
 
-#include "interface.hpp"
-
 namespace pid
 {
+/**
+ * @brief A Controller class derived from any combination of P, I and D types.
+ * 
+ * Types that are inherited must also inherit from Base which guarantee the
+ * Control() function implementation. Only one of each type can be used with the
+ * same typename parameter or a compilation error will occur.
+ * 
+ * @tparam C Comma-separated list of P, I and D types
+ */
 template<class... C>
 class Controller : public C...
 {
-using T = typename Controller::_Type_;
+using T = typename Controller::_Type_; /**< Inherited data type */
 
 public:
+    /**
+     * @brief Compute each control type and return its sum.
+     * 
+     * @param err Controller input
+     * @return T Controller output
+     */
     inline T Loop(T err)
     {
         return (C::Control(err) + ...);
     }
 };
 }
+
+#include "interface.hpp"
 
 #endif/*__CONTROLLER_HPP__*/
--- pid/controller/interface.hpp
+++ pid/controller/interface.hpp
@@ -1,47 +1,99 @@
+/**
+ * @file interface.hpp
+ * @author Benjamin von Snarski (benjaminvonsnarski@gmail.com)
+ * @brief Header file for PID definition and base interface.
+ * @version 0.1
+ * @date 07-12-2022
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef __INTERFACE_HPP__
 #define __INTERFACE_HPP__
 
 namespace pid
 {
+/**
+ * @brief An abstract base class to interface with the Controller class.
+ * 
+ * @tparam T Data type
+ */
 template<typename T>
 class Base
 {
 protected:
-    using _Type_ = T;
+    using _Type_ = T; /**< Inheritable data type */
 protected:
+    /**
+     * @brief The control function to be implemented.
+     * 
+     * @param err Controller input
+     * @return T Controller output
+     */
     virtual inline T Control(T err) = 0;
 };
 
+/**
+ * @brief The proportional control type.
+ * 
+ * @tparam T Data type
+ */
 template<typename T>
 class P : protected Base<T>
 {
 public:
-    T Kp;
+    T Kp; /**< Proportional gain */
 
 protected:
+    /**
+     * @brief The proportional control function.
+     * 
+     * @param err Controller input
+     * @return T Controller output
+     */
     inline T Control(T err) override
     {
         return Kp * err;
     }
 };
 
+/**
+ * @brief The integral control type.
+ * 
+ * @tparam T Data type
+ */
 template<typename T>
 class I : protected Base<T>
 {
 public:
-    T Ki;
-    T Time;
+    T Ki; /**< Integral gain */
+    T Time; /**< Total time elapsed */
 
 public:
+    /**
+     * @brief Returns the total error.
+     * 
+     * @return T Sum of error
+     */
     inline T GetTotalError() const
     {
         return m_Sum;
     }
 
 protected:
-    T m_Sum;
+    T m_Sum; /**< Sum of error */
 
 protected:
+    /**
+     * @brief The integral control function.
+     * 
+     * Must update the Time field before each function call.
+     * Internally updates the total error.
+     * 
+     * @see Time
+     * @param err Control input
+     * @return T Control output
+     */
     inline T Control(T err) override
     {
         m_Sum += err;
@@ -49,23 +101,42 @@ protected:
     }
 };
 
+/**
+ * @brief The derivative control type.
+ * 
+ * @tparam T Data type
+ */
 template<typename T>
 class D : protected Base<T>
 {
 public:
-    T Kd;
-    T Period;
+    T Kd; /**< Derivative gain */
+    T Period; /**< Sampling period */
 
 public:
+    /**
+     * @brief Returns the previous error.
+     * 
+     * @return T Previous error
+     */
     inline T GetPrevError() const
     {
         return m_Err;
     }
 
 protected:
-    T m_Err;
+    T m_Err; /**< Previous error */
 
 protected:
+    /**
+     * @brief The derivative control function.
+     * 
+     * Must set the Period field before initial function call.
+     * Internally updates the previous error.
+     * 
+     * @param err Control input
+     * @return T Control output
+     */
     inline T Control(T err) override
     {
         T prev_err = m_Err;
