aac1c9091ddea1e2dc4e7b73bba2f13b1d7cd6f1
benvonh <benjaminvonsnarski@gmail.com>
2023-02-24 13:52:56 +1000

    reformat

--- pid/controller/controller.hpp
+++ pid/controller/controller.hpp
@@ -17,6 +17,15 @@ public:
         return (C::Loop(err) + ...);
     }
 };
+
+template<typename T = double>
+using PI = Controller<P<T>, I<T>>;
+
+template<typename T = double>
+using PD = Controller<P<T>, D<T>>;
+
+template<typename T = double>
+using PID = Controller<P<T>, I<T>, D<T>>;
 }
 
 #endif/*__PID__CONTROLLER_HPP__*/
--- pid/pid.hpp
+++ pid/pid.hpp
@@ -3,16 +3,4 @@
 
 #include "controller/controller.hpp"
 
-namespace pid
-{
-template<typename T = double>
-using PI = Controller<P<T>, I<T>>;
-
-template<typename T = double>
-using PD = Controller<P<T>, D<T>>;
-
-template<typename T = double>
-using PID = Controller<P<T>, I<T>, D<T>>;
-};
-
 #endif/*__PID_HPP__*/
